a2ed0c232ed427e9f0047745cd9d70274f2fa47b
gunnarebeling <gunnarebeling@icloud.com>
2025-01-22 09:53:31 -0600

    added better auth

--- src/components/auth/AuthorizedRoute.jsx
+++ src/components/auth/AuthorizedRoute.jsx
@@ -1,17 +1,25 @@
 /* eslint-disable react/prop-types */
 import { jwtDecode } from "jwt-decode";
 import { Navigate } from "react-router-dom";
+import { verifyUser } from "../../managers/authManager";
 
 
 // This component returns a Route that either display the prop element
 // or navigates to the login. If roles are provided, the route will require
 // all of the roles when all is true, or any of the roles when all is false
-export const AuthorizedRoute = ({ children, roles }) => {
+export const AuthorizedRoute =  async ({ children, roles }) => {
+  let verified = false;
+
   const token = localStorage.getItem('token');
   let authed = false;
   let loggedInUser = null;
+  await verifyUser().then((res) => {
+    if (res) {
+      res.status === 200 ? verified = true : verified = false;
+    }
+  });
 
-  if (token) {
+  if (token && verified) {
     try {
       loggedInUser = jwtDecode(token);
       if (loggedInUser && roles) {
